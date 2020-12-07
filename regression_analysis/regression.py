96927334e3eec11314adcc995b1370f7726a6e23
Josh <jlovering@wpi.edu>
2020-12-07 18:17:14 -0500

    final datset

--- regression_analysis/regression.py
+++ regression_analysis/regression.py
@@ -9,11 +9,13 @@ import matplotlib.pyplot as plt
 def main():
     np.random.seed(r) # set random seed
     ## import data ##
-    data = pd.read_csv("clean_data/final_data.csv")
+    data = pd.read_csv("Clean_Data/final_data.csv")
     # TODO, adjust this to fit the actual final data csv
-    X = data.iloc[:,:] # feature columns
-    y = data.iloc[:,:] # label column
+    X = data.iloc[:,5:11] # feature columns
+    y = data.iloc[:,2:4] # label column
+    y = data['Depression_Score'] # label column
 
+    print(X.columns)
     ## train test split ##
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size= 0.2)
@@ -21,7 +23,8 @@ def main():
     ## Regression ##
     regr = linear_model.LinearRegression()
     # regr = linear_model.LogisticRegression()
-    regr.fit(X_train, y_train) # train on training data
+    lm = regr.fit(X_train, y_train) # train on training data
+    score = regr.score(X, y)
     y_pred = regr.predict(X_test) # test on testing data
 
     ## Analyze Results ##
@@ -32,8 +35,10 @@ def main():
     print('Coefficient of determination: %.2f'
         % r2_score(y_test, y_pred))
 
+    print("R^2: ", score)
+
     ## Plot outputs ##
-    # plt.scatter(X_test, y_test,  color='black')
+    # plt.scatter(X_test, y_pred,  color='black')
     # plt.plot(X_test, y_pred, color='blue', linewidth=3)
     # plt.xticks(())
     # plt.yticks(())
