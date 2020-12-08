62af24901df50b1bc23270e08a29017cb0e3de50
Josh <jlovering@wpi.edu>
2020-12-07 19:21:03 -0500

    normalized final data

--- regression_analysis/regression.py
+++ regression_analysis/regression.py
@@ -10,40 +10,47 @@ def main():
     np.random.seed(r) # set random seed
     ## import data ##
     data = pd.read_csv("Clean_Data/final_data.csv")
-    # TODO, adjust this to fit the actual final data csv
-    X = data.iloc[:,5:11] # feature columns
-    y = data.iloc[:,2:4] # label column
-    y = data['Depression_Score'] # label column
+    states = data['State'].unique()
 
-    print(X.columns)
-    ## train test split ##
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size= 0.2)
+    for state in states:
+        state_data = data.copy().loc[data['State'] == state]
+        
+        if state_data.shape[0] == 0:
+            continue
 
-    ## Regression ##
-    regr = linear_model.LinearRegression()
-    # regr = linear_model.LogisticRegression()
-    lm = regr.fit(X_train, y_train) # train on training data
-    score = regr.score(X, y)
-    y_pred = regr.predict(X_test) # test on testing data
+        X = state_data.iloc[:,5:] # feature columns
+        y = state_data.iloc[:,2:4] # label column
 
-    ## Analyze Results ##
-    print('Coefficients: \n', regr.coef_) # The coefficients
-    print('Mean squared error: %.2f'
-        % mean_squared_error(y_test, y_pred)) # The mean squared error
-    # The coefficient of determination: 1 is perfect prediction
-    print('Coefficient of determination: %.2f'
-        % r2_score(y_test, y_pred))
+        ## train test split ##
 
-    print("R^2: ", score)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size= 0.2)
 
-    ## Plot outputs ##
-    # plt.scatter(X_test, y_pred,  color='black')
-    # plt.plot(X_test, y_pred, color='blue', linewidth=3)
-    # plt.xticks(())
-    # plt.yticks(())
-    # plt.savefig("regression_analysis/charts/regression.png")
-    # plt.show()
+        ## Regression ##
+        regr = linear_model.LinearRegression()
+        # regr = linear_model.LogisticRegression()
+        lm = regr.fit(X_train, y_train) # train on training data
+        score = regr.score(X, y)
+        y_pred = regr.predict(X_test) # test on testing data
+
+        ## Analyze Results ##
+        # print('Coefficients: \n', regr.coef_) # The coefficients
+        # print('Mean squared error: %.2f'
+            # % mean_squared_error(y_test, y_pred)) # The mean squared error
+        # The coefficient of determination: 1 is perfect prediction
+        # print('Coefficient of determination: %.2f'
+            # % r2_score(y_test, y_pred))
+
+        print("State: ", state)
+        print("R^2: ", score)
+
+        ## Plot outputs ##
+        # plt.scatter(X_test, y_pred,  color='black')
+        # plt.plot(X_test, y_pred, color='blue', linewidth=3)
+        # plt.xticks(())
+        # plt.yticks(())
+        # plt.savefig("regression_analysis/charts/regression.png")
+        # plt.show()
 
 if __name__ == "__main__":
     ## initialize variables ## 
